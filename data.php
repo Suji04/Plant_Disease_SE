36637e454e7ff35320ce1cfac412fb8f272181ca
Sujan Dutta <sujan99dutta@gmail.com>
2018-12-28 00:05:05 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,4 +1,11 @@
-<table border='1' cellpadding='10'>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Search Engine</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<table id="tt">
     <tr> 
     	<th>NAME</th>
     	<th>CAUSE</th>
@@ -9,24 +16,23 @@
 
 	$k = $_POST['k'];
 
-	//if((!empty($year)) && (!empty($name))){
 	$host = "localhost";
-	$dbname = "firsttable";
+	$dbname = "plant_disease";
 
-	$link = mysqli_connect($host, $dbname);
+	$link = mysqli_connect($host,"root","", $dbname);
 
-	if(mysqli_connect_errno())
-		{die("Connect Error");}
-	$db_selected = mysqli_select_db($link, "firsttable");
-	if (!$db_selected) {
-		die ('Can\'t use foo : ' . mysqli_error($link));
-	}
+	// if(mysqli_connect_errno())
+	// 	{die("Connect Error");}
+	// $db_selected = mysqli_select_db($link, "plant_disease");
+	// if (!$db_selected) {
+	// 	die ('Can\'t use foo : ' . mysqli_error($link));
+	// }
+
 	$terms = explode(" ", $k);
 	$sql = "SELECT * FROM plant_disease WHERE ";
 	$i = 0;
 	foreach ($terms as $each)
 	{
-		//echo $each;
 		$i++;
 
 		if($i == 1){
@@ -36,31 +42,33 @@
 			$sql .= "OR keywords LIKE '%$each%' ";
 		}
 	}
-	//echo $sql;
-	if($sql === FALSE) {die();}
-	$result = mysqli_query($link, $sql);
-	if($result === FALSE) {
-		echo "error: \n", mysqli_error($link); 
-		die();
-	}
+
+	// if($sql === FALSE) {die();}
+	// $result = mysqli_query($link, $sql); 
+	// if($result === FALSE) {
+	// 	echo "error: \n", mysqli_error($link); 
+	// 	die();
+	// }
+	
+	$result = mysqli_query($link, $sql); 
 	if(mysqli_num_rows($result) == 0)
 		{ echo "No results found!"; }
 	else {
 		while($row = mysqli_fetch_array($result))
 		{
 			echo "<tr>";
-			echo "<th>".$row['name']."</th>&nbsp;";
-			echo "<th>".$row['cause']."</th>&nbsp;";
-			echo "<th>".$row['symptoms']."</th>&nbsp;";
-			echo "<th>".$row['treatment']."</th>&nbsp;";
+			echo "<td>".$row['name']."</td>&nbsp;";
+			echo "<td>".$row['cause']."</td>&nbsp;";
+			echo "<td>".$row['symptoms']."</td>&nbsp;";
+			echo "<td>".$row['treatment']."</td>&nbsp;";
 			echo "</tr>";
 		}
 	}
-
-	//}
-
 	mysqli_close($link);
-
 ?>
 
 </table>
+</body>
+</html>
+
+
